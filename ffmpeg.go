85c2c71b1067ce1eda34718124a20773c5fb6734
ashraf <ashraf1abdelrasool@gmail.com>
2019-05-07 10:30:20 +0200

    init

--- ffmpeg.go
+++ ffmpeg.go
@@ -64,6 +64,6 @@ func Webp(url string, ffprobe FFprobe, gcsEvent GCSEvent) ([]string, string, err
 func Hls(url string, ffprobe FFprobe, gcsEvent GCSEvent) ([]string, string, error) {
 	dir, _ := createDir(gcsEvent.Pattern + "/" + GetName(gcsEvent.Name))
 	name := getBaseName(GetName(gcsEvent.Name))
-	message, err := ffmpeg("-y", "-v", "error", "-i", url, "-hls_segment_type", "mpegts", "-f", "hls", "-codec:", "copy", "-start_number", "0", "-hls_time", "2", "-hls_list_size", "0", "-hls_enc", "1", "-hls_enc_key", gcsEvent.Meta.Key, "-hls_enc_key_url", name+".key", "-hls_enc_iv", gcsEvent.Meta.Iv, "-master_pl_name", name+".m3u8", dir+"/"+name+"_"+".m3u8")
+	message, err := ffmpeg("-y", "-v", "error", "-i", url, "-hls_segment_type", "mpegts", "-f", "hls", "-codec:", "copy", "-start_number", "0", "-hls_time", "2", "-hls_list_size", "0", "-hls_enc", "1", "-hls_enc_key", gcsEvent.Meta.Key, "-hls_enc_key_url", dir+"/"+name+".key", "-hls_enc_iv", gcsEvent.Meta.Iv, "-master_pl_name", name+".m3u8", dir+"/"+name+"_"+".m3u8")
 	return walk(dir), message, err
 }
